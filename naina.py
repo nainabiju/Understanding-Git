0053f33304692f6e1611f105ef86302aa99f0221
nainabiju <jagnes007@gmail.com>
2024-10-02 22:40:43 +0530

    This is my source code

---
+++ naina.py
@@ -0,0 +1,2 @@
+"Hello world this is my first python file "
+"thiis is my new version"
--- naina.sh
+++ naina.sh
@@ -4,3 +4,4 @@ echo "Hi my name is naina"
 echo "What is your name"
 read name
 ##############Write your name"######################
+echo "Naina"
